8a6c1b9776cb5a26486d370640d53699949c1c1f
logicise <tech@logicise.pl>
2025-01-23 18:54:38 +0100

    Update files to sync with the out folder

---
+++ _next/static/yMFJRBXtG_UKrBgLH-Bwm/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog\u002F[postSlug]","\u002Fblog\u002Fauthor\u002F[authorSlug]","\u002Fblog\u002Ftag\u002F[tagSlug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
